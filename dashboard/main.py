0626dbdd14af75ebb03d842d236b07c58a25418c
Lucas Malheiros <lgdm1303@gmail.com>
2023-03-27 18:19:46 -0300

    Ajustes código

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import dash
 import dash_bootstrap_components as dbc
 from dash import dcc, html, Input, Output, callback
-# from datetime import date, datetime
+from datetime import date, datetime
 from time import gmtime, strftime
 from df_manipulation import clean_original_data, clean_arena_data
 
@@ -35,7 +35,8 @@ app.layout = dbc.Container([
                 min_date_allowed=min(df["date"]),
                 max_date_allowed=max(df["date"]),
                 initial_visible_month=max(df["date"]),
-                date=max(df["date"])
+                date=max(df["date"]),
+                display_format='DD/MM/Y'
             ), width=6, className="text-center"),
         dbc.Col(
             dcc.Slider(
@@ -47,39 +48,39 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(
             dbc.Card(
-                [dbc.CardHeader(html.H2("Atendimento em até 1 minuto (%)")),
-                 dbc.CardBody(html.H3(id="output-percent-atendimento"))]
+                [dbc.CardHeader(html.H3("Atendimento em até 1 minuto (%)")),
+                 dbc.CardBody(html.H2(id="output-percent-atendimento"))]
                     ), width=6, className="text-center"),
         dbc.Col(
             dbc.Card(
-                [dbc.CardHeader(html.H2("Número de chamados no dia")),
-                 dbc.CardBody(html.H3(id="output-chamados"))]
+                [dbc.CardHeader(html.H3("Número de chamados no dia")),
+                 dbc.CardBody(html.H2(id="output-chamados"))]
                     ), width=6, className="text-center"),
     ]),
     # Linha 4 - KPIs de tempo médio de atendimento
     dbc.Row([
         dbc.Col(
             dbc.Card(
-                [dbc.CardHeader(html.H2("Tempo médio de atendimento (min)")),
-                 dbc.CardBody(html.H3(id="output-media-atendimento"))]
+                [dbc.CardHeader(html.H3("Tempo médio de atendimento (min)")),
+                 dbc.CardBody(html.H2(id="output-media-atendimento"))]
                     ), width=6, className="text-center"),
         dbc.Col(
             dbc.Card(
-                [dbc.CardHeader(html.H2("Tempo médio de espera (min)")),
-                 dbc.CardBody(html.H3(id="output-media-espera"))]
+                [dbc.CardHeader(html.H3("Tempo médio de espera (min)")),
+                 dbc.CardBody(html.H2(id="output-media-espera"))]
                     ), width=6, className="text-center"),
     ]),
     # Linha 5 - Taxa de abandono ("service_length" < 30s)
     dbc.Row([
         dbc.Col(
             dbc.Card(
-                [dbc.CardHeader(html.H2("Taxa de desistência (%)")),
-                 dbc.CardBody(html.H3(id="output-percent-desistencia"))]
+                [dbc.CardHeader(html.H3("Taxa de desistência (%)")),
+                 dbc.CardBody(html.H2(id="output-percent-desistencia"))]
                     ), width=6, className="text-center"),
         dbc.Col(
             dbc.Card(
-                [dbc.CardHeader(html.H2("Utilização operadores (%)")),
-                 dbc.CardBody(html.H3(id="output-utilizacao"))]
+                [dbc.CardHeader(html.H3("Utilização operadores (%)")),
+                 dbc.CardBody(html.H2(id="output-utilizacao"))]
                     ), width=6, className="text-center"),
     ]),
 ], className="mt-3")
@@ -98,23 +99,23 @@ app.layout = dbc.Container([
      Output("output-utilizacao", "children")],
     [Input("my-date-picker", "date")]
 )
-def update_kpis(date):
+def update_kpis(data):
     """Calcula os KPIs de acordo com a data."""
     # Percentual atendido em até 1 minuto
-    dff = df.loc[df["date"] == date]
+    dff = df.loc[df["date"] == data]
     percent = dff["meets_standard"].mean()
     # Chamados por dia
-    callers = df.groupby(["date"])["daily_caller"].max()[date]
+    callers = df.groupby(["date"])["daily_caller"].max()[data]
     # Média tempo de atendimento
-    media_atendimento = df.groupby(["date"])["service_length"].mean()[date]
+    media_atendimento = df.groupby(["date"])["service_length"].mean()[data]
     media_atendimento = strftime("%M:%S", gmtime(media_atendimento))
     # Média tempo de espera
-    media_espera = df.groupby(["date"])["wait_length"].mean()[date]
+    media_espera = df.groupby(["date"])["wait_length"].mean()[data]
     media_espera = strftime("%M:%S", gmtime(media_espera))
     # Taxa de desistência
     if len(dff) > 0:
         taxa_desistencia = len(df.loc[(df["service_length"] < 30) &
-                                      (df["date"] == date)]) / len(dff)
+                                      (df["date"] == data)]) / len(dff)
     else:
         taxa_desistencia = 0
     # Percentual de utilização
