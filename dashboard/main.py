56ec11644433849cb0fc53eed603471320819906
Lucas Malheiros <lgdm1303@gmail.com>
2023-03-27 23:29:15 -0300

    Reformatação do código

--- dashboard/main.py
+++ dashboard/main.py
@@ -10,8 +10,6 @@ from time import gmtime, strftime
 from df_manipulation import clean_original_data, clean_arena_data
 
 
-
-
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # --------------------------------------------------------------------------- #
@@ -60,12 +58,11 @@ app.layout = dbc.Container([
                  dbc.CardBody(html.H2(id="output-chamados"))]
                     ), width=6, className="text-center"),
     ]),
-    #linha 3.5 - Gráficos e picker de gráficos para cada um dos KPIS
+    # Linha 3.5 - Gráficos e picker de gráficos para cada um dos KPIS
     dbc.Row([
         dbc.Col(dcc.Graph(id="grafico-percentil")),
         dbc.Col(dcc.Graph(id="grafico-chamados")),
-    ]),    
-
+    ]),
     # Linha 4 - KPIs de tempo médio de atendimento
     dbc.Row([
         dbc.Col(
@@ -98,23 +95,20 @@ app.layout = dbc.Container([
 # --------------------------------------------------------------------------- #
 # Callbacks
 
-
+# KPIs
 @app.callback(
     [Output("output-percent-atendimento", "children"),
      Output("output-chamados", "children"),
      Output("output-media-atendimento", "children"),
      Output("output-media-espera", "children"),
      Output("output-percent-desistencia", "children"),
-     Output("output-utilizacao", "children"),
-     Output("grafico-percentil","figure"),
-     Output("grafico-chamados","figure")],
+     Output("output-utilizacao", "children")],
+    #  Output("grafico-percentil", "figure"),
+    #  Output("grafico-chamados", "figure")],
     [Input("my-date-picker", "date")]
 )
 def update_kpis(data):
     """Calcula os KPIs de acordo com a data."""
-    if data == "2021-12-31T00:00:00":
-        data = "2021-12-31"
-
     # Percentual atendido em até 1 minuto
     dff = df.loc[df["date"] == data]
     percent = dff["meets_standard"].mean()
@@ -122,9 +116,8 @@ def update_kpis(data):
     callers = df.groupby(["date"])["daily_caller"].max()[data]
     # Média tempo de atendimento
     media_atendimento = df.groupby(["date"])["service_length"].mean()[data]
-    media_atendimento = strftime("%M:%S", gmtime(media_atendimento)) 
-    
-        # Média tempo de espera
+    media_atendimento = strftime("%M:%S", gmtime(media_atendimento))
+    # Média tempo de espera
     media_espera = df.groupby(["date"])["wait_length"].mean()[data]
     media_espera = strftime("%M:%S", gmtime(media_espera))
     # Taxa de desistência
@@ -137,22 +130,38 @@ def update_kpis(data):
     n_atendentes = 4
     horas_disponiveis = 10 * 60 * 60 * n_atendentes
     utilizacao = dff["service_length"].sum() / horas_disponiveis
-
-    # Gráficos para cada um dos KPIS
-    mes = df.loc[df["date"].dt.month == datetime.strptime(data, '%Y-%m-%d').month]
-    # percentual 
-    percent_graph = px.bar(mes.groupby(["date"]), x = mes["date"].unique(),y = round(mes.groupby(["date"])["meets_standard"].mean(),2), height = 275)
-
-    # numero de atendimentos 
-    
-    callers_graph = px.bar(mes.groupby(["date"]), x = mes["date"].unique(), y = mes.groupby(["date"])["daily_caller"].max(), height = 275)
-    #atendimentos = px.bar(media_atendimento, x = date, y = )
-
     # Retorna os valores
     return (f"{percent * 100 :.2f}%", f"{callers}",
             f"{media_atendimento}", f"{media_espera}",
-            f"{taxa_desistencia * 100 :.2f}%", f"{utilizacao * 100 :.2f}%",
-            percent_graph, callers_graph)
+            f"{taxa_desistencia * 100 :.2f}%", f"{utilizacao * 100 :.2f}%")
+
+
+# Gráficos
+@app.callback(
+    [Output("grafico-percentil", "figure"),
+     Output("grafico-chamados", "figure")],
+    [Input("my-date-picker", "date")]
+)
+def update_figures(data):
+    """Função de callback dos gráficos dos KPIs."""
+    if data == "2021-12-31T00:00:00":
+        data = "2021-12-31"
+    # Gráficos para cada um dos KPIS
+    mes = df.loc[df["date"].dt.month == datetime.strptime(data,
+                                                          '%Y-%m-%d').month]
+    # Percentual
+    percent_graph = px.bar(mes.groupby(["date"]),
+                           x=mes["date"].unique(),
+                           y=round(mes.groupby(["date"])
+                                   ["meets_standard"].mean(), 2),
+                           height=275)
+    # Número de atendimentos
+    callers_graph = px.bar(mes.groupby(["date"]),
+                           x=mes["date"].unique(),
+                           y=mes.groupby(["date"])["daily_caller"].max(),
+                           height=275)
+    # Atendimentos = px.bar(media_atendimento, x = date, y = )
+    return percent_graph, callers_graph
 
 
 # --------------------------------------------------------------------------- #
